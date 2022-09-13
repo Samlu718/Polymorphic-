c4eb20102930f9f954892f0d66ead292bda31e65
Samlu718 <t111318098@ntut.org.tw>
2022-09-13 22:20:11 +0800

    data 操控

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,35 @@ Route::get('/post/{id}/{name}', [PostController::class, 'show_post']);
 Route::get('/contact', [PostController::class, 'contact']);
 
 Route::get('/home', [PostController::class, 'home']);
+
+
+Route::get('/insert',function () {
+
+    DB::insert('insert into posts(title, body) values(?,?)',['PHP with Laravel','Laravel is the best thing that has happened to PHP']);
+
+});
+
+Route::get('/read',function () {
+
+    $results = DB::select('select * from posts where id =?',[2]);
+
+    return var_dump($results);
+
+    // foreach($results as $post){
+    //     return $post->title;
+    // }
+
+});
+
+
+Route::get('/update',function () {
+
+    $update = DB::update('update posts set title = "Update title" where id=?',[2]);
+    return $update;
+});
+
+Route::get('/delete',function () {
+
+    $delete = DB::delete('delete from posts where id=?',[2]);
+    return $delete;
+});
